933c983978d6c80a6857caa0639da9a6be9ef7e6
WangNing-Zhou <11344283+wangning-zhou@user.noreply.gitee.com>
2022-12-27 01:47:41 +0800

    Initial commit

---
+++ mybatis_helloworld/src/main/java/com/zhouzhou/mybatis/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.zhouzhou.mybatis.mapper;
+
+/**
+ * @author 周万宁
+ * @className UserMapper
+ * @create 2022/12/27-1:46
+ * @description 用户mapper
+ */
+public interface UserMapper {
+
+    int insertUser();
+
+
+}
