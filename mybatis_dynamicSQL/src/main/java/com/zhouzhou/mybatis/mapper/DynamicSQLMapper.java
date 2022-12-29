6c269f4e16579a4fe52e19e519e0d9550979fadf
WangNing-Zhou <11344283+wangning-zhou@user.noreply.gitee.com>
2022-12-30 03:29:37 +0800

    mybatis学习

--- mybatis_dynamicSQL/src/main/java/com/zhouzhou/mybatis/mapper/DynamicSQLMapper.java
+++ mybatis_dynamicSQL/src/main/java/com/zhouzhou/mybatis/mapper/DynamicSQLMapper.java
@@ -1,5 +1,10 @@
 package com.zhouzhou.mybatis.mapper;
 
+import com.zhouzhou.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 /**
  * @author 周万宁
  * @className DynamicSQLMapper
@@ -7,4 +12,15 @@ package com.zhouzhou.mybatis.mapper;
  * @description 动态sql映射接口
  */
 public interface DynamicSQLMapper {
+
+    /**
+     * @MethodName getEmpByCondition
+     * @Author 周万宁
+     * @Description TODO
+     * @Date 2:45 2022/12/30
+     * @Param [emp]
+     * @return java.util.List<com.zhouzhou.mybatis.pojo.Emp>
+     **/
+    List<Emp> getEmpByCondition( Emp emp);
+
 }
---
+++ mybatis_dynamicSQL/src/test/java/com/zhouzhou/mybatis/test/DynamicSQLTest.java
@@ -0,0 +1,27 @@
+package com.zhouzhou.mybatis.test;
+
+import com.zhouzhou.mybatis.mapper.DynamicSQLMapper;
+import com.zhouzhou.mybatis.pojo.Emp;
+import com.zhouzhou.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author 周万宁
+ * @className DynamicSQLTest
+ * @create 2022/12/30-2:47
+ * @description 动态sql测试
+ */
+public class DynamicSQLTest {
+    @Test
+    public void testGetEmpByCondition(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+        Emp emp = new Emp(null,"张三",20,"");
+        List<Emp> empByCondition = mapper.getEmpByCondition(emp);
+        System.out.println(empByCondition);
+    }
+
+}
