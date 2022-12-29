d943e97b652aac6fdc4fb513f979cc81cbcc6b6c
WangNing-Zhou <11344283+wangning-zhou@user.noreply.gitee.com>
2022-12-30 02:44:15 +0800

    mybatis学习

---
+++ mybatis_dynamicSQL/src/main/java/com/zhouzhou/mybatis/mapper/DynamicSQLMapper.java
@@ -0,0 +1,10 @@
+package com.zhouzhou.mybatis.mapper;
+
+/**
+ * @author 周万宁
+ * @className DynamicSQLMapper
+ * @create 2022/12/30-2:43
+ * @description 动态sql映射接口
+ */
+public interface DynamicSQLMapper {
+}
---
+++ mybatis_dynamicSQL/src/main/java/com/zhouzhou/mybatis/pojo/Dept.java
@@ -0,0 +1,56 @@
+package com.zhouzhou.mybatis.pojo;
+
+import java.util.List;
+
+/**
+ * @author 周万宁
+ * @className Dept
+ * @create 2022/12/29-3:05
+ * @description 部门类
+ */
+public class Dept {
+    private Integer deptId;
+    private String deptName;
+    private List<Emp> emps;
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "deptId=" + deptId +
+                ", deptName='" + deptName + '\'' +
+                ", emps=" + emps +
+                '}';
+    }
+
+    public List<Emp> getEmps() {
+        return emps;
+    }
+
+    public void setEmps(List<Emp> emps) {
+        this.emps = emps;
+    }
+
+    public Dept() {
+    }
+
+    public Dept(Integer deptId, String deptName) {
+        this.deptId = deptId;
+        this.deptName = deptName;
+    }
+
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+}
