6a713a4a902fd60191599fb0c1cedfbc66ef5620
WangNing-Zhou <1879154660@qq.com>
2023-01-01 21:50:42 +0800

    Spring学习

---
+++ spring_mvc_helloworld/src/main/java/com/zhouzhou/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.zhouzhou.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author 周万宁
+ * @className HelloController
+ * @create 2023/1/1-21:37
+ * @description
+ */
+@Controller
+public class HelloController {
+
+
+
+}
