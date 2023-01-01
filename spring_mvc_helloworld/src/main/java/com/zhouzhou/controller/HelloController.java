46f644b6e3be57c6cd1fcfff6fffd8a7a899b9b7
WangNing-Zhou <1879154660@qq.com>
2023-01-01 22:15:04 +0800

    SpringMVC学习

--- spring_mvc_helloworld/src/main/java/com/zhouzhou/controller/HelloController.java
+++ spring_mvc_helloworld/src/main/java/com/zhouzhou/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.zhouzhou.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author 周万宁
@@ -11,6 +12,18 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class HelloController {
 
+    @RequestMapping("/")
+    public String protal(){
+
+        //将逻辑视图返回
+        return"index";
+
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "success";
+    }
 
 
 }
