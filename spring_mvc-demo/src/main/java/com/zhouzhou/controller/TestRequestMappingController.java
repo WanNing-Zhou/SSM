e3be50a969e1720817f91512129bfdea5acb5f83
WangNing-Zhou <1879154660@qq.com>
2023-01-02 00:46:18 +0800

    SpringMVC学习

--- spring_mvc-demo/src/main/java/com/zhouzhou/controller/TestRequestMappingController.java
+++ spring_mvc-demo/src/main/java/com/zhouzhou/controller/TestRequestMappingController.java
@@ -1,6 +1,7 @@
 package com.zhouzhou.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -51,9 +52,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * 若当前请求满足@RequestMapping注解的value和method属性，但是不满足headers属性，此时页面
  * 显示404错误，即资源未找到
  *
+ * 6.SpringMVC支持ant风格的路径
+ * 在@RequestMapping注解的value属性值设置一些特殊字符
+ * ?:任意的单个字符(不包括?)
+ * *:任意个数的任意字符
+ * **:任意层数的任意目录,注意使用方式只能 **写在双斜线中,斜线里不能有任何的其他字符
+ *
+ *7.@RequestMapping注解使用路径中的占位符
+ * 传统:/deleteUser?id=1
+ * rest:/user/delete/1
+ * 需要在@RequestMapping注解的value属性中所设置的路径中,使用{XXX}的方式来表示路径中的数据
+ * 在通过@PathVariable注解,将占位符所标识的值和控制器方法的形参进行绑定
+ *
+ *
+ *
  */
+
+
 @Controller
-@RequestMapping("/test")
+//@RequestMapping("/test")
 public class TestRequestMappingController {
     //此时控制器方法所匹配的请求的请求路径设置为/test/hello
     @RequestMapping(
@@ -65,4 +82,21 @@ public class TestRequestMappingController {
     public String hello(){
         return "success";
     }
+
+
+    @RequestMapping("/a?a/test/ant")
+    public String testAnt(){
+        return "success";
+    }
+
+
+    @RequestMapping("/test/rest/{username}/{id}")
+    public String testRest(@PathVariable("id") Integer id,@PathVariable("username") String username){
+
+        System.out.println("username"+username);
+        System.out.println("id"+id);
+        return "success";
+    }
+
+
 }
