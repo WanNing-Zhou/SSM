a347dff928cf64afe718a15359320c249a011b8d
WangNing-Zhou <1879154660@qq.com>
2023-01-02 18:15:04 +0800

    SpringMVC学习

--- spring_mvc_rest/src/main/java/com/zhouzhou/controller/TestRestController.java
+++ spring_mvc_rest/src/main/java/com/zhouzhou/controller/TestRestController.java
@@ -1,9 +1,7 @@
 package com.zhouzhou.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author 周万宁
@@ -16,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
  * 修改用户信息-->/user-->put
  * 删除用户信息-->/user/1-->delete
  *
+ * 注意:浏览器目前只能发送get和post请求
+ * 若要发送put和delete请求,需要在web.xml配置一个过滤器HiddenHttpMethodFilter
+ * 配置了过滤器之后,发送的请求需要满足两个条件,才能将请求方式转换为put或delete
+ * 1.当前请求必须为post
+ * 2.当前请求必须传输请求参数_method,._method的值才是最终的请求方式
  *
  *
  */
@@ -23,15 +26,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class TestRestController {
 
-    @RequestMapping(value = "",method = RequestMethod.GET)
+//    @RequestMapping(value = "",method = RequestMethod.GET)
+    @GetMapping("")
     public String getAllUser(){
         System.out.println("查询所有用户信息-->/user-->get");
         return "success";
     }
 
-   @RequestMapping(value = "/${id}",method = RequestMethod.GET)
-   public String getUserById(){
-       System.out.println("根据id查询用户信息-->/user/1-->get");
+//   @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    @GetMapping("/{id}")
+   public String getUserById(@PathVariable("id") Integer id){
+       System.out.println("根据id查询用户信息-->/user/"+id+"-->get");
+        return "success";
+    }
+
+//    @RequestMapping(value = "",method = RequestMethod.POST)
+    @PostMapping("")
+    public String insertUser(){
+        System.out.println(" * 添加用户信息-->/user/-->post");
+        return "success";
+    }
+
+
+//    @RequestMapping(value = "",method = RequestMethod.PUT)
+    @PutMapping
+    public String updaeUser(){
+        System.out.println(" 修改用户信息-->/user/-->put");
+        return "success";
+    }
+
+
+//    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    @DeleteMapping
+    public String deleteUserById(@PathVariable("id") Integer id){
+        System.out.println("根据id删除用户信息-->/user/"+id+"-->delete");
         return "success";
     }
 
