07b95754d46fab17f62406cb3bff764d823d094b
WangNing-Zhou <1879154660@qq.com>
2023-01-02 22:50:11 +0800

    SpringMVC学习

---
+++ spring_mvc_ajax/src/main/java/com/zhouzhou/controller/TestAjaxController.java
@@ -0,0 +1,47 @@
+package com.zhouzhou.controller;
+
+import com.zhouzhou.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author 周万宁
+ * @className TestAjaxController
+ * @create 2023/1/2-20:42
+ * @description
+ * 1.@RequestBody:将亲求题中的内容和控制器方法的形参进行绑定
+ * 2.@RequestBody注解将json格式的请求参数转换成java对象
+ * a>jackson的依赖
+ * b>在SpringMVC的配置文件中设置<mvc:annotation-drivern/>
+ * c>在处理请求的控制器方法的形参位置,直接设置json格式的请求参数要转换的java类型的形参,使用@requestbody注解即可
+ */
+@Controller
+public class TestAjaxController {
+
+    @RequestMapping(value = "/test/ajax",method = RequestMethod.POST)
+    public void testAjax(Integer id, @RequestBody String requstBody, HttpServletResponse response) throws IOException {
+        System.out.println(requstBody);
+        System.out.println(id);
+        response.getWriter().write("hello,axios");
+    }
+    @RequestMapping(value = "/test/requestbody/json",method = RequestMethod.POST)
+    public void testRequestBody(@RequestBody User user,HttpServletResponse response) throws IOException {
+        System.out.println(user);
+        response.getWriter().write("hello,requestBody");
+
+    }
+
+    public void testRequestBody(@RequestBody Map<String,Object> map, HttpServletResponse response) throws IOException {
+        System.out.println(map);
+        response.getWriter().write("hello,requestBody");
+
+    }
+
+}
