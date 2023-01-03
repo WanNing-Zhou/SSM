99428ab0b8ca9a4dab9288e20b328d570e4af95d
WangNing-Zhou <1879154660@qq.com>
2023-01-03 19:04:59 +0800

    SSM整合学习

---
+++ ssm/src/main/java/com/zhouzhou/ssm/mapper/EmployeeMapper.java
@@ -0,0 +1,12 @@
+package com.zhouzhou.ssm.mapper;
+
+/**
+ * @author 周万宁
+ * @className EmployeeMapper
+ * @create 2023/1/3-18:18
+ * @description
+ */
+public interface EmployeeMapper {
+
+
+}
--- ssm/src/main/java/com/zhouzhou/ssm/service/impl/EmployeeServiceImpl.java
+++ ssm/src/main/java/com/zhouzhou/ssm/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.zhouzhou.ssm.service.impl;
 
 import com.zhouzhou.ssm.service.EmployeeService;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author 周万宁
@@ -10,7 +13,9 @@ import org.springframework.stereotype.Service;
  * @description
  */
 @Service
+@Transactional
 public class EmployeeServiceImpl implements EmployeeService {
+
 
 
 
